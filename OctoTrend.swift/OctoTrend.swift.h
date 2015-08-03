1a4d8de5e0160858b5736cc3b3fa19d85a8512a4
Benedikt Terhechte <terhechte@gmail.com>
2015-08-03 16:18:01 +0200

    Initial Commit

---
+++ OctoTrend.swift/OctoTrend.swift.h
@@ -0,0 +1,19 @@
+//
+//  OctoTrend.swift.h
+//  OctoTrend.swift
+//
+//  Created by Benedikt Terhechte on 03/08/15.
+//  Copyright © 2015 Benedikt Terhechte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OctoTrend.swift.
+FOUNDATION_EXPORT double OctoTrend_swiftVersionNumber;
+
+//! Project version string for OctoTrend.swift.
+FOUNDATION_EXPORT const unsigned char OctoTrend_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OctoTrend_swift/PublicHeader.h>
+
+
