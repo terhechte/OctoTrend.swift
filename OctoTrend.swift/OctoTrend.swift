0cb032543addc5f4e6322aedd6bcebd1ec457939
Benedikt Terhechte <terhechte@gmail.com>
2015-08-03 16:30:36 +0200

    Added Carthage & Kanna

---
+++ OctoTrend.swift/OctoTrend.swift
@@ -0,0 +1,10 @@
+//
+//  OctoTrend.swift
+//  OctoTrend.swift
+//
+//  Created by Benedikt Terhechte on 03/08/15.
+//  Copyright © 2015 Benedikt Terhechte. All rights reserved.
+//
+
+import Foundation
+import Kanna
