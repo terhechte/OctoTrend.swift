d2b2686d7096cc965364ecbe73364ecdbcfeef09
Benedikt Terhechte <terhechte@gmail.com>
2015-08-08 20:37:09 +0200

    Added primary entrance for retrieving just the data.

--- OctoTrend.swift/OctoTrend.swift
+++ OctoTrend.swift/OctoTrend.swift
@@ -9,6 +9,8 @@
 import Foundation
 import Kanna
 
+let kGithubTrendsURLTemplate = "https://github.com/trending?l=%@&since=%@"
+
 struct User {
     let name: String
     let imageURL: NSURL
@@ -29,4 +31,26 @@ struct Repository {
     let stars: Stars
     let text: String
     let starred: Bool
+}
+
+enum TrendingTimeline : String {
+    case Today = "daily"
+    case Week = "weekly"
+    case Month = "monthly"
+}
+func trends(language language: String, timeline: TrendingTimeline,
+    completion: (result: [Repository]?) -> ()) {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) { () -> Void in
+        guard let url = NSURL(string: String(format: kGithubTrendsURLTemplate, language, timeline.rawValue))
+            else { completion(result: nil); return }
+        let request = NSURLRequest(URL: url)
+        
+        do {
+            let data = try NSURLConnection.sendSynchronousRequest(request, returningResponse: nil)
+            
+            completion(result: parseTrendsHTML(data))
+        } catch _ {
+            completion(result: nil)
+        }
+    }
 }
